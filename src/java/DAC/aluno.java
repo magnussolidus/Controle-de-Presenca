86072a8f7db40676cc224aee274be79183d47c51
magnussolidus <magnussolidus@gmail.com>
2019-12-20 17:33:44 -0300

    mudança de inteiro para booleano

--- src/java/DAC/aluno.java
+++ src/java/DAC/aluno.java
@@ -3,19 +3,19 @@ package DAC;
 public class aluno {
 
     String name;
-    int[] code;
+    boolean[] code;
 
     public aluno(String name) {
         this.name = name;
-        this.code = new int[30];
+        this.code = new boolean[30];
         
         for(int i=0; i<30; i++)
         {
-            code[i] = -1; // computando como falta;
+            code[i] = false; // computando como falta;
         }
     }
     
-    public void addCode(int toAdd, int id)
+    public void addCode(boolean toAdd, int id)
     {
         this.code[id] = toAdd;
     }
